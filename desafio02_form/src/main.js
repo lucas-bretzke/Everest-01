e676380dd3147353a28b575495667b8f375f1826
Lucas Bretzke <lucas.bretzke@warren.com.br>
2022-05-30 10:45:03 -0300

    add icons

--- desafio02_form/src/main.js
+++ desafio02_form/src/main.js
@@ -10,10 +10,10 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon, } from '@fortawesome/vue-fontawesome'
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
-import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faXmark, faAngleDown, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 
-library.add(faEnvelope, faWhatsapp, faXmark)
+library.add(faEnvelope, faWhatsapp, faXmark, faAngleDown, faArrowLeft)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 
