5bbef97dbfc7a5b7aa896c12736546538e81e75c
Lucas Bretzke <lucas.bretzke@warren.com.br>
2022-05-31 21:06:44 -0300

    add icon in main

--- desafio02_form/src/main.js
+++ desafio02_form/src/main.js
@@ -10,10 +10,10 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon, } from '@fortawesome/vue-fontawesome'
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
-import { faXmark, faAngleDown, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faXmark, faAngleDown, faArrowLeft, faLock } from '@fortawesome/free-solid-svg-icons'
 
 
-library.add(faEnvelope, faWhatsapp, faXmark, faAngleDown, faArrowLeft)
+library.add(faEnvelope, faWhatsapp, faXmark, faAngleDown, faArrowLeft, faLock)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 
