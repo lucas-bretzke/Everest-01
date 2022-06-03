0764f6a56c6e3d1fad286197e9723edfbc431200
Lucas Bretzke <lucas.bretzke@warren.com.br>
2022-06-03 00:44:50 -0300

    button return ready

--- desafio02_form/src/components/btnReturn.vue
+++ desafio02_form/src/components/btnReturn.vue
@@ -1,42 +1,50 @@
 <template>
-  <button v-on:click="runRoute">
-    <font-awesome-icon icon="fa-solid fa-arrow-left" />
-  </button>
+  <font-awesome-icon
+    icon="fa-solid fa-arrow-left"
+    v-on:click="previousPage"
+    class="ic-arrow-left"
+  />
 </template>
 
 
 <script>
 export default {
-  name: "btnReturn",
-  data() {
-    return {
-        a: this.$router.push
-    };
+  name: "BtnReturn",
+  props: {
+    previousPageUrl: { type: String },
   },
-  // props: {
-  //   route: {
-  //     type: String,
-  //     default: "",
-  //   },
-  // },
-  // method: {
-  //   runRoute() {
-  //     this.$router.push({
-  //       name: route,
-  //     });
-  //   },
-  // },
+  data() {
+    return {};
+  },
+  methods: {
+    previousPage() {
+      this.$router.push(this.previousPageUrl);
+    },
+  },
 };
 </script>
 
 
 <style scoped>
-button {
+.ic-arrow-left {
+  width: 22px;
+  height: 22px;
+  padding: 5px;
+
+  top: 15px;
+  left: 15px;
   position: fixed;
-  margin: 15px;
 
   font-size: 13px;
 
+  border: none;
+  border-radius: 50%;
   cursor: pointer;
+  color: white;
+  background-color: transparent;
+}
+
+.ic-arrow-left:hover {
+  background-color: rgba(255, 255, 255, 0.27);
 }
 </style>
