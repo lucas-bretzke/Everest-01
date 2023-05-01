522a440355f35180f8789174870991a9c6ac2851
Lucas Bretzke <lucasbretzke@gmail.com>
2023-04-30 21:16:07 -0300

    feat: added function validEmail

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -32,6 +32,9 @@ const cpf = (cpf) => {
     cpf = cpf.replace(/[^\d]/g, "");
     return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")
 }
-
+const validEmail = (email) => {
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
 
 export { validar, phone, truncate, cpf }
